ec5ae35f2bdbd87e569a147f3c0a01e00bfe863e
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-02-21 06:57:25 +0400

    Update settings.py

--- dive_club/dive_club/settings.py
+++ dive_club/dive_club/settings.py
@@ -91,6 +91,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
